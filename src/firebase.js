ba3ce5a536db948b848eadb5748947df035efa7b
vitshan77 <vithushanarul08@gmail.com>
2023-08-09 16:10:18 +0200

    project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC-x85HFKqOxCylEMWfk-tbbq-m1liJ5Y",
+    authDomain: "netflix-51dd8.firebaseapp.com",
+    projectId: "netflix-51dd8",
+    storageBucket: "netflix-51dd8.appspot.com",
+    messagingSenderId: "715199146659",
+    appId: "1:715199146659:web:0687a8912cc81933706d16"
+};
